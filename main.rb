450f579c4b7bc09ff65ce37fd46d13e9a1fb4a61
luq <luqman.yazrie@outlook.com>
2024-04-26 03:34:05 +0000

    OS-specific clearing, and modulus calculation

--- main.rb
+++ main.rb
@@ -1,11 +1,28 @@
+require 'rbconfig'
+
+def clear
+    os = RbConfig::CONFIG['host_os']
+    if os.include?('linux')
+        system('clear')
+    
+    elsif os.include?('mingw') || os.include?("mswin")
+        system('cls')
+    
+    end
+end
+
+# Variables
 n1 = 0
 n2 = 0
 ans = 0
 ans_log = []
 vars = Hash.new
-loop do
-    system("cls")
 
+# Main loop
+loop do
+    clear()
+
+    # Actions available
     puts "Actions available:"
     puts "  - calc      | Calculator"
     puts "  - var       | Variable manager"
@@ -19,31 +36,38 @@ loop do
     if action == "calc"
         # First question, repeats until correct input is provided
         loop do
-            system("cls")
+            clear()
 
             puts "Enter the first input"
             puts "-"
             n1 = gets.chomp.downcase
 
+            # Checking if integer or float
             if n1.to_i.to_s == n1
                 puts "integer"
                 n1 = n1.to_i
                 break
+
             elsif n1.to_f.to_s == n1
                 puts "float"
                 n1 = n1.to_f
                 break
+
             else
+                # Using variables
+
                 if n1 == "ans"
                     puts "Using default ans variable with #{ans} value"
                     n1 = ans
                     sleep(2)
                     break
+
                 elsif vars.key?(n1)
                     puts "Using #{n1} variable, with #{vars[n1]} value"
                     n1 = vars[n1]
                     sleep(2)
                     break
+
                 else
                     puts "Not a valid input - Only existing variables, ints, floats and ans are supported"
                 end
@@ -54,31 +78,37 @@ loop do
 
         # Second question, repeats until correct input is provided
         loop do
-            system ("cls")
+            clear()
 
             puts "Enter the second input"
             puts "-"
             n2 = gets.chomp.downcase
 
+            # Check if integer or float
             if n2.to_i.to_s == n2
                 puts "integer"
                 n2 = n2.to_i
                 break
+
             elsif n2.to_f.to_s == n2
                 puts "float"
                 n2 = n2.to_f
                 break
+
             else
+                # Using variables
                 if n2 == "ans"
                     puts "Using default ans variable, with #{ans} value"
                     n2 = ans
                     sleep(2)
                     break
+
                 elsif vars.key?(n2)
                     puts "Using #{n2} variable, with #{vars[n2]} value"
                     n2 = vars[n2]
                     sleep(2)
                     break
+
                 else
                     puts "Not a valid input - Only existing variables, ints, floats and ans are supported"
                 end
@@ -87,7 +117,7 @@ loop do
             puts
         end
 
-        system("cls")
+        clear()
 
         # Operation options
         puts "Select one from the following options:"
@@ -95,26 +125,36 @@ loop do
         puts "  - sub   | Subtraction"
         puts "  - div   | Division"
         puts "  - mul   | Multiplication"
+        puts "  - mod   | Modulus"
         puts "-"
         operation = gets.chomp.downcase
 
         # Math operations
         if operation == "add"
             ans = n1 + n2
-            system("cls")
+            clear()
             puts "Result of #{n1} + #{n2} = #{ans}"
+
         elsif operation == "sub"
             ans = n1 - n2
-            system("cls")
+            clear()
             puts "Result of #{n1} - #{n2} = #{ans}"
+
         elsif operation == "div"
             ans = n1 / n2
-            system("cls")
+            clear()
             puts "Result of #{n1} / #{n2} = #{ans}"
+
         elsif operation == "mul"
             ans = n1 * n2
-            system("cls")
+            clear()
             puts "Result of #{n1} * #{n2} = #{ans}"
+        
+        elsif operation == "mod"
+            ans = n1 % n2
+            clear()
+            puts "Result of #{n1} * #{n2} = #{ans}"
+
         else
             puts "Unknown operation"
         end
@@ -123,7 +163,7 @@ loop do
 
     # Variable function
     elsif action == "var"
-        system("cls")
+        clear()
 
         # Enter variable name
         puts "Enter variable name"
@@ -133,7 +173,7 @@ loop do
 
         # Enter variable data
         loop do
-            system("cls")
+            clear()
 
             puts "Enter variable data"
             puts "-"
@@ -166,7 +206,7 @@ loop do
 
         # Check if variable is already defined
         if vars.has_key?(var_name)
-            system("cls")
+            clear()
 
             puts "Variable #{var_name} already defined, overwrite value? (y/n)"
             puts "-"
@@ -186,7 +226,7 @@ loop do
 
     # Ans Log
     elsif action == "log"
-        system("cls")
+        clear()
 
         puts "Answer log"
         if ans_log.length == 0
@@ -201,14 +241,14 @@ loop do
         
     # Quit
     elsif action == "quit"
-        system("cls")
+        clear()
 
         puts "Exiting..."
         break
     
     # 404
     else
-        system("cls")
+        clear()
 
         puts "Unknown action - action does not exist"
     end
